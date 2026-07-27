8d37aedf68f4914ac40c79ffa7dbae5445eeba51
Taryn Huang <9huangt@gmail.com>
2026-07-27 15:23:07 -0400

    added handoff subsystem

---
+++ src/main/java/com/stuypulse/robot/subsystems/handoff/Handoff.java
@@ -0,0 +1,53 @@
+package com.stuypulse.robot.subsystems.handoff;
+
+import org.littletonrobotics.junction.Logger;
+
+import com.stuypulse.robot.constants.Settings;
+import com.stuypulse.robot.subsystems.handoff.HandoffIO.HandoffIOOutputs;
+import com.stuypulse.robot.subsystems.intake.Intake;
+import com.stuypulse.robot.subsystems.intake.IntakeIO;
+import com.stuypulse.robot.subsystems.intake.IntakeIO.IntakeIOOutputs;
+import com.stuypulse.robot.subsystems.intake.IntakeIOSim;
+import com.stuypulse.robot.subsystems.intake.IntakeIOTalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Handoff extends SubsystemBase{
+    private static final Handoff instance;
+
+    static {
+    if (Settings.currentMode == Settings.Mode.SIM) {
+      instance = new Handoff(new HandoffIOSim());
+    } else {
+      instance = new Handoff(new HandoffIOTalonFX());
+    }
+  }
+
+    public static Handoff getInstance() {
+    return instance;
+  }
+  
+    private final HandoffIO io;
+    private final HandoffIOInputsAutoLogged inputs;
+    private final HandoffIOOutputs outputs;
+
+    private Handoff(HandoffIO io) {
+        this.io = io;
+        this.inputs = new HandoffIOInputsAutoLogged;
+        this.outputs = new HandoffIOOutputs;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Handoff", inputs);
+  }
+
+    public void periodicAfterScheduler() {
+        io.applyOutputs(outputs);
+  }
+  
+    private void stopMotors() {
+        outputs.handoffDutyCycle = 0.0;
+  }
+}
