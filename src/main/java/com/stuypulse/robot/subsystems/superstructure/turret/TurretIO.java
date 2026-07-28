84857c16b5760f8f48ce349177d902648a6c968c
grainbao <thisnameiswrong@gmail.com>
2026-07-28 17:31:14 -0400

    ADD: added turretIO and TalonFX files

--- src/main/java/com/stuypulse/robot/subsystems/superstructure/turret/TurretIO.java
+++ src/main/java/com/stuypulse/robot/subsystems/superstructure/turret/TurretIO.java
@@ -1,5 +1,36 @@
 package com.stuypulse.robot.subsystems.superstructure.turret;
 
-public class TurretIO {
-    
+import org.littletonrobotics.junction.AutoLog;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+
+
+public interface TurretIO {
+    @AutoLog
+    class TurretIOInputs{
+        public Current turretMotorSupplyCurrent = Amps.zero();
+        public Current turretMotorStatorCurrent = Amps.zero();
+        public Temperature turrentMotorTemperature = Celsius.zero();
+        public Angle turrentMotorPosition = Degrees.zero();
+        public Voltage turrentMotorAppliedVoltage = Volts.zero();
+        public AngularVelocity turretMotorVelocity = DegreesPerSecond.zero();
+    }
+
+    class TurretIOOutputs {
+        public Angle turretPosition = Degrees.zero();
+    }
+
+    public default void updateInputs(TurretIOInputs inputs){}
+
+    public default void applyOutputs(TurretIOOutputs outputs) {}
+
 }
--- src/main/java/com/stuypulse/robot/subsystems/superstructure/turret/TurretIOTalonFX.java
+++ src/main/java/com/stuypulse/robot/subsystems/superstructure/turret/TurretIOTalonFX.java
@@ -1,5 +1,62 @@
 package com.stuypulse.robot.subsystems.superstructure.turret;
 
-public class TurretIOTalonFX {
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.stuypulse.robot.constants.Ports;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+
+public class TurretIOTalonFX implements TurretIO {
+    private final TalonFX turretMotor;
+
+    private final PositionVoltage positionController;
+
+    private final StatusSignal<Angle> turretMotorPosition;
+    private final StatusSignal<Current> turretMotorSupplyCurrent;
+    private final StatusSignal<Current> turretMotorStatorCurrent;
+    private final StatusSignal<Temperature> turretMotorTemperature;
+    private final StatusSignal<Voltage> turretMotorAppliedVoltage;
+    private final StatusSignal<AngularVelocity> turretMotorVelocity;
+
+    public TurretIOTalonFX(){
+        turretMotor = new TalonFX(Ports.Superstructure.Turret.MOTOR, Ports.RIO);
+
+        positionController = new PositionVoltage(0).withEnableFOC(true);
+
+        turretMotorPosition = turretMotor.getPosition();
+        turretMotorSupplyCurrent = turretMotor.getSupplyCurrent();
+        turretMotorStatorCurrent = turretMotor.getStatorCurrent();
+        turretMotorTemperature = turretMotor.getDeviceTemp();
+        turretMotorAppliedVoltage = turretMotor.getMotorVoltage();
+        turretMotorVelocity = turretMotor.getVelocity();
+    }
+
+    @Override
+    public void updateInputs(TurretIOInputs inputs) {
+        BaseStatusSignal.refreshAll(
+            turretMotorPosition,
+            turretMotorSupplyCurrent,
+            turretMotorStatorCurrent,
+            turretMotorTemperature,
+            turretMotorAppliedVoltage,
+            turretMotorVelocity);
     
+        inputs.turrentMotorPosition = turretMotorPosition.getValue();
+        inputs.turretMotorSupplyCurrent = turretMotorSupplyCurrent.getValue();
+        inputs.turretMotorStatorCurrent = turretMotorStatorCurrent.getValue();
+        inputs.turrentMotorTemperature = turretMotorTemperature.getValue();
+        inputs.turrentMotorAppliedVoltage = turretMotorAppliedVoltage.getValue();
+        inputs.turretMotorVelocity = turretMotorVelocity.getValue();
+    }
+
+    @Override
+    public void applyOutputs(TurretIOOutputs outputs) {
+        turretMotor.setControl(positionController.withPosition(outputs.turretPosition));
+    }
 }
